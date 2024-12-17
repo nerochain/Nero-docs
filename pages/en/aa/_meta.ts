dffb5b8d6d87b0ffe4e3ff581fe1746f9f2a3c2e
yuu11111 <yuusan3777@gmail.com>
2024-12-17 17:48:43 +0900

    feature walle ui

--- pages/en/aa/_meta.ts
+++ pages/en/aa/_meta.ts
@@ -2,8 +2,9 @@ export default {
   architecture: "Architecture",
   paymaster: "Paymaster",
   userOpSdkUsage: "UerOp-SDK Usage",
-  aaWallet: "AA Wallet Usage",
   aaPlatformUsageGuide: "AA Platform Usage",
+  aaWallet: "AA Wallet Usage",
+  aaWalletUi: "AA Wallet UI Usage",
   accessEntryPoint: "Access Entry Point"
   
 }
