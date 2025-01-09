283293470ddd60aee84e8ba502752d8b80abc606
yuu11111 <yuusan3777@gmail.com>
2025-01-09 10:36:19 +0900

    disable aawallet

--- pages/ja/aa/_meta.ts
+++ pages/ja/aa/_meta.ts
@@ -3,7 +3,10 @@ export default {
   paymaster: "Paymaster",
   userOpSdkUsage: "UerOp-SDKの使用方法",
   aaPlatformUsageGuide: "AAプラットフォーム使用ガイド",
+  aaWallet: {
+    display: 'hidden'
+    //"AAウォレットの使用方法"
+  },
   aaWalletUi: "AAウォレットのUI使用方法",
-  accessEntryPoint: "エントリーポイント",
-  // aaWallet: "AAウォレットの使用方法",
+  accessEntryPoint: "エントリーポイント"
 }
