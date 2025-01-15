95e93d32264991c5c153a538c214bbfd1f3e6186
yuu11111 <yuusan3777@gmail.com>
2025-01-15 14:05:44 +0900

    disable zk

--- pages/en/learn/_meta.ts
+++ pages/en/learn/_meta.ts
@@ -1,12 +1,17 @@
 export default {
-    introduction: "1 Introduction",
-    architectures: "2 Architecture",
-    zkRollupExecutionEngine: "3 Zk Rollup Execution Engine",
-    consensus: "4 Consensus",
-    dataAvailabilitySharding: "5 Data Availability Sharding",
-    nativeAccountAbstractionSupport: "6 Native Account Abstraction Support",
-    feeSharingMechanismInEcosystem: "7 Fee-sharing Mechanism in Ecosystem",
-    futureWork: "8 Future Work",
-    references: "References"
-  }
-  
+  introduction: "1 Introduction",
+  architectures: "2 Architecture",
+  zkRollupExecutionEngine: {
+    display: "hidden",
+    //"3 Zk Rollup Execution Engine"
+  },
+  consensus: "3 Consensus",
+  dataAvailabilitySharding: "4 Data Availability Sharding",
+  nativeAccountAbstractionSupport: "5 Native Account Abstraction Support",
+  feeSharingMechanismInEcosystem: "6 Fee-sharing Mechanism in Ecosystem",
+  futureWork:{
+    display: "hidden",
+    // "7 Future Work",
+  },
+  references: "References",
+};
--- pages/ja/learn/architectures/_meta.ts
+++ pages/ja/learn/architectures/_meta.ts
@@ -1,7 +1,10 @@
 export default {
   architecture: "2 Architecture",
   accessLayer: "2.1 Access Layer: Native Account Abstraction",
-  executionLayer: "2.2 Execution layer: Built-in ZK Rollup Execution Engine",
+  executionLayer:{
+    display: "hidden",
+    //"2.2 Execution layer: Built-in ZK Rollup Execution Engine"
+  },
   settlementLayer:
     "2.3 Settlement layer: High-performance EVM-compatible Chain",
   dataAvailabilityLayer: "2.4 Data Availability Layer: Storage Scalability",
