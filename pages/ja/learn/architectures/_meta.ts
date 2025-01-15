6c95c76c717ab5a9dc2d7cbaad429ccdea1da73d
yuu11111 <yuusan3777@gmail.com>
2025-01-15 15:42:29 +0900

    change section number

--- pages/ja/learn/architectures/_meta.ts
+++ pages/ja/learn/architectures/_meta.ts
@@ -2,6 +2,6 @@ export default {
   architecture: "2 Architecture",
   accessLayer: "2.1 Access Layer: Native Account Abstraction",
   settlementLayer:
-    "2.3 Settlement layer: High-performance EVM-compatible Chain",
-  dataAvailabilityLayer: "2.4 Data Availability Layer: Storage Scalability",
+    "2.2 Settlement layer: High-performance EVM-compatible Chain",
+  dataAvailabilityLayer: "2.3 Data Availability Layer: Storage Scalability",
 };
