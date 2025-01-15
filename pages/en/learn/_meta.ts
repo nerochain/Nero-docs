7c514acd4f7e7e8f78e8d47f355b133c6d89edcc
yuu11111 <yuusan3777@gmail.com>
2025-01-15 15:41:06 +0900

    move to retreat

--- pages/en/learn/_meta.ts
+++ pages/en/learn/_meta.ts
@@ -1,17 +1,9 @@
 export default {
   introduction: "1 Introduction",
   architectures: "2 Architecture",
-  zkRollupExecutionEngine: {
-    display: "hidden",
-    //"3 Zk Rollup Execution Engine"
-  },
   consensus: "3 Consensus",
   dataAvailabilitySharding: "4 Data Availability Sharding",
   nativeAccountAbstractionSupport: "5 Native Account Abstraction Support",
   feeSharingMechanismInEcosystem: "6 Fee-sharing Mechanism in Ecosystem",
-  futureWork:{
-    display: "hidden",
-    // "7 Future Work",
-  },
   references: "References",
 };
