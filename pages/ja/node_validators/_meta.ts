fa4a8539f4ef0c39650aaa9b2010a0f4bc3f4e2e
yuu11111 <yuusan3777@gmail.com>
2025-02-10 15:42:01 +0900

    add json rpc

--- pages/ja/node_validators/_meta.ts
+++ pages/ja/node_validators/_meta.ts
@@ -2,5 +2,6 @@ export default {
   overview: "Overview",
   compileAndRun: "Compile And Run",
   runningValidatorNode: "Running Validator Node",
+  jsonRpc: "JSON-RPC",
   commandLineOptions: "Command Line Options",
 };
