a534f7f221d6d29d8c0d71ef7fdce37d0e43b5b6
Marcus Souza <souza.mvsl@gmail.com>
2025-03-05 13:38:17 -0300

    feat: add placeholder files to the architecture section

--- pages/en/nero-architecture/_meta.ts
+++ pages/en/nero-architecture/_meta.ts
@@ -1,3 +1,33 @@
 export default {
-  index: 'Overview'
+  '*': {
+    theme: {
+      toc: true,
+      breadcrumb: true
+    }
+  },
+  index: 'Overview',
+  'core-concepts': {
+    title: 'Core Concepts',
+    type: 'folder',
+    theme: {
+      toc: true,
+      breadcrumb: true
+    }
+  },
+  'consensus-mechanism': {
+    title: 'Consensus Mechanism',
+    type: 'folder',
+    theme: {
+      toc: true,
+      breadcrumb: true
+    }
+  },
+  'data-availability': {
+    title: 'Data Availability & Sharding',
+    type: 'folder',
+    theme: {
+      toc: true,
+      breadcrumb: true
+    }
+  }
 } 
