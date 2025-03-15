0659e6faea94df3bf99739957d9dfb4e09a99b6e
Marcus Souza <souza.mvsl@gmail.com>
2025-03-15 16:43:29 -0300

    feat: add japanese translations

---
+++ pages/ja/tutorials/first-contract/_meta.ts
@@ -0,0 +1,13 @@
+export default {
+  '*': {
+    theme: {
+      layout: 'default'
+    }
+  },
+  'using-remix': 'Remixを使用',
+  'using-hardhat': 'Hardhatを使用',
+  'using-foundry': 'Foundryを使用',
+  'interacting-with-contract': 'コントラクトとの対話',
+  'testing-contract': 'コントラクトのテスト',
+  'deploying-to-mainnet': 'メインネットへのデプロイ'
+} 
