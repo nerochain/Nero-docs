633aa8d5c6052be66dda1a8c9afde6862006d4fa
Marcus Souza <souza.mvsl@gmail.com>
2025-05-01 19:53:17 -0300

    feat: adds japanese translation

--- pages/ja/tutorials/low-level/_meta.ts
+++ pages/ja/tutorials/low-level/_meta.ts
@@ -1,6 +1,10 @@
 export default {
-  index: 'クックブック',
-  'first-contract': 'NERO Chainで最初のコントラクトをデプロイする',
+  '*': {
+    theme: {
+      toc: true,
+      breadcrumb: true
+    }
+  },
   'aa-wallet-integration': 'AAウォレット連携',
   'sending-ops': 'UserOperationsの送信',
   'types-payments': '支払い方法',
