20da75075648fa4d72b1edab2af0be3bc57a524a
Marcus Souza <souza.mvsl@gmail.com>
2025-05-01 16:09:51 -0300

    feat: adds nero-wallet as high level approach. Restructures the docs tutorials in low and high level

---
+++ pages/en/tutorials/high-level/_meta.ts
@@ -0,0 +1,11 @@
+export default {
+  '*': {
+    theme: {
+      toc: true,
+      breadcrumb: true
+    }
+  },
+  'nero-wallet-architecture': 'Configuring and Understanding NERO Wallet and Paymaster',
+  'nero-wallet-template-quickstart': 'From Template to dApp'
+  
+} 
---
+++ pages/en/tutorials/low-level/_meta.ts
@@ -0,0 +1,13 @@
+export default {
+  '*': {
+    theme: {
+      toc: true,
+      breadcrumb: true
+    }
+  },
+  'aa-wallet-integration': 'AA Wallet Integration',
+  'sending-ops': 'Sending UserOperations',
+  'types-payments': 'Payment Methods',
+  'checking-sup-tokens': 'Token Support Check',
+  'create-first-dapp': 'Create Your First dApp'
+} 
